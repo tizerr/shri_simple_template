42f07ea909213c55c9e565ea69ea8057a2c3246f
anton-zm <antonzm34@gmail.com>
2022-10-27 21:31:42 +0300

    review commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
+import { useEffect } from 'react';
 import { TodoList } from '../components/TodoList';
 
 export const Home = () => {
+    useEffect(() => {
+        alert('Good job!')
+        console.log('Congratulation!')
+    },[])
     return (
         <>
             <h1 data-testid="page-title">Home</h1>
             <p>This is the list.</p>
             <TodoList />
+            <h2>Blabla</h2>
         </>
     );
 };
